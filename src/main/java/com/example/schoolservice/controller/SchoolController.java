65963dcf70cc887bff2d1cfa8c1ec22bff07fa64
Likhi-G1 <likhithagopireddy2001@gmail.com>
2024-12-17 18:52:30 +0530

    Testing purpose

--- src/main/java/com/example/schoolservice/controller/SchoolController.java
+++ src/main/java/com/example/schoolservice/controller/SchoolController.java
@@ -1,9 +1,12 @@
 package com.example.schoolservice.controller;
 
 import com.example.schoolservice.entity.SchoolExcursion;
+import com.example.schoolservice.entity.Student;
 import com.example.schoolservice.service.SchoolService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -13,6 +16,27 @@ public class SchoolController {
 
     @Autowired
     private SchoolService schoolService;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/findHealthStudentStatus")
+    public String getHealthStatusFromStudentService() {
+        String studentServiceUrl = "http://localhost:8081/api/student/healthStatus";
+        return restTemplate.getForObject(studentServiceUrl, String.class);
+    }
+
+    @PostMapping("/registerStudent")
+    public ResponseEntity<?> registerStudent(@RequestBody Student student) {
+        // Assuming StudentService is running on http://localhost:8082
+        String studentServiceUrl2 = "http://localhost:8081/api/student/add";
+
+        // Make the HTTP POST call to StudentService
+        ResponseEntity<?> response = restTemplate.postForEntity(studentServiceUrl2, student, Object.class);
+
+        // Return the response received from StudentService
+        return ResponseEntity.status(response.getStatusCode()).body(response.getBody());
+    }
 
     @PostMapping("/register")
     public String registerStudent(@RequestBody SchoolExcursion student) {
---
+++ src/main/java/com/example/schoolservice/entity/Student.java
@@ -0,0 +1,8 @@
+package com.example.schoolservice.entity;
+
+public class Student {
+    private Long id;
+    private String name;
+    private String standard;
+    private String email;
+}
